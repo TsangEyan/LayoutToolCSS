31eb8195a7a11fdf04eac4e37915b3d74709ad48
Tsang <10265841+tsangyiyan@user.noreply.gitee.com>
2022-10-18 20:04:59 +0800

    start

---
+++ layoutTool/urls.py
@@ -0,0 +1,8 @@
+from xml.etree.ElementInclude import include
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+    path('mytool/', include('mytool.urls')),
+]
